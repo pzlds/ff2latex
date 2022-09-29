ff3884432077f1521233bd69a3ae5f955a175622
pzlds <pzlds@users.noreply.github.com>
2022-09-29 18:32:39 +0000

    Clean up spaces in between triple dots

--- ff2latex.py
+++ ff2latex.py
@@ -27,7 +27,8 @@ CLEANUP_REPLACEMENTS = {
     r'[^\S\r\n]+}[^\S\r\n]*': r'} ',
     r'[^\S\r\n]+([,.?!])': r'\1',
     r'\\emph{\\emph{([^{}]+)}}': r'\\emph{\1}',
-    r'[^\S\r\n]+([\r\n])': r'\1'
+    r'[^\S\r\n]+([\r\n])': r'\1',
+    r'\. \. \.': r'...',
 }
 
 def pure_children(element):
